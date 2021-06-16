df7a9c55f4b19f248ca8b72827cc41c17765c560
jacek-domanski <j.domanski92@gmail.com>
2021-06-16 18:44:57 +0200

    Initial commit

---
+++ src/main/java/com/jd/redah/RecreationalDataHarvesterApplication.java
@@ -0,0 +1,13 @@
+package com.jd.redah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecreationalDataHarvesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecreationalDataHarvesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jd/redah/RecreationalDataHarvesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jd.redah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecreationalDataHarvesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
